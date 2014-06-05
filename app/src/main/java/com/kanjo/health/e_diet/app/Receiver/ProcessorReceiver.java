9c29d10f6bb01eb9a08d2ff52dbd800e49d18cc8
JARP <ramirezp.jorge@gmail.com>
2014-06-05 00:24:02 -0500

    Adding default extra to get message

--- app/src/main/java/com/kanjo/health/e_diet/app/Receiver/ProcessorReceiver.java
+++ app/src/main/java/com/kanjo/health/e_diet/app/Receiver/ProcessorReceiver.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
 import com.kanjo.health.e_diet.app.PagerMainActivity;
@@ -17,9 +18,11 @@ public class ProcessorReceiver implements IProcessorMessage {
 
     //TODO we need to do more things than just send notifications
     Context mContext ;
+    Intent mIntent;
     @Override
     public void processMessage(Intent intent, Context context) {
         mContext = context;
+        mIntent= intent;
         if(intent!=null)
             postNotification();
     }
@@ -29,14 +32,23 @@ public class ProcessorReceiver implements IProcessorMessage {
         final NotificationManager mNotificationManager =
                 (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        String message =null;
+        if (mIntent.getAction().equals("com.google.android.c2dm.intent.RECEIVE")) {
+            Bundle extras = mIntent.getExtras();
+            message= extras.getString("default");
+        }
+
         Intent mIntent = new Intent(mContext, PagerMainActivity.class);
 
         final PendingIntent mPendingIntent =
                 PendingIntent.getActivity(mContext,0,mIntent, Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL);
 
+        if(message==null)
+            message="Error no message receive";
         Notification mNotification = new NotificationCompat.Builder(mContext)
                                         .setSmallIcon(R.drawable.ic_launcher)
-                                        .setContentText("e-Diet")
+                                        .setContentTitle("e-Diet has a new message")
+                                        .setContentText(message)
                                         .setContentIntent(mPendingIntent)
                                         .setAutoCancel(true)
                                         .build();
